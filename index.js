67232233f022dc24e7c6d0bc90f5abe3ea5d7376
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2022-05-11 17:33:05 +0300

    feat: add movement for player and enemy

--- index.js
+++ index.js
@@ -6,35 +6,43 @@ canvas.height = 576;
 
 canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.2;
+const gravity = 0.7;
+const speed = 5;
+const bounce = -15;
 
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
     this.height = 150;
+    this.width = 50;
+    this.lastKey;
   }
 
   draw() {
     canvasContext.fillStyle = "red";
-    canvasContext.fillRect(this.position.x, this.position.y, 50, this.height);
+    canvasContext.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
   }
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.velocity.y = 0;
-    } else {
-      this.velocity.y += gravity;
-    }
+    } else this.velocity.y += gravity;
   }
 }
 
 const player = new Sprite({
   position: {
-    x: 0,
+    x: 100,
     y: 0,
   },
   velocity: {
@@ -45,7 +53,7 @@ const player = new Sprite({
 
 const enemy = new Sprite({
   position: {
-    x: 400,
+    x: canvas.width - 150,
     y: 100,
   },
   velocity: {
@@ -54,10 +62,28 @@ const enemy = new Sprite({
   },
 });
 
-// player.draw();
-// enemy.draw();
-
 console.log(player);
+
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    counter: 0,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+  ArrowUp: {
+    counter: 0,
+  },
+};
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -65,6 +91,89 @@ function animate() {
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  player.velocity.x = 0;
+  enemy.velocity.x = 0;
+
+  //player movement
+  if (keys.a.pressed && player.lastKey === "a") {
+    player.velocity.x = -speed;
+  } else if (keys.d.pressed && player.lastKey === "d") {
+    player.velocity.x = speed;
+  }
+
+  //enemy movement
+  if (keys.ArrowLeft.pressed && enemy.lastKey === "ArrowLeft") {
+    enemy.velocity.x = -speed;
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
+    enemy.velocity.x = speed;
+  }
 }
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+  console.log("keydown", event.key, keys.w.counter);
+  switch (event.key) {
+    //player keys
+    case "d":
+      keys.d.pressed = true;
+      player.lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      player.lastKey = "a";
+      break;
+    case "w":
+      if (player.velocity.y === 0) {
+        keys.w.counter = 0;
+        player.velocity.y = bounce;
+        keys.w.counter++;
+      } else if (player.velocity.y !== 0 && keys.w.counter < 2) {
+        player.velocity.y = bounce;
+        keys.w.counter++;
+      }
+      break;
+
+    //enemy keys
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      enemy.lastKey = "ArrowRight";
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      enemy.lastKey = "ArrowLeft";
+      break;
+    case "ArrowUp":
+      if (enemy.velocity.y === 0) {
+        keys.ArrowUp.counter = 0;
+        enemy.velocity.y = bounce;
+        keys.ArrowUp.counter++;
+      } else if (enemy.velocity.y !== 0 && keys.ArrowUp.counter < 2) {
+        enemy.velocity.y = bounce;
+        keys.ArrowUp.counter++;
+      }
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  console.log("keyup", event.key);
+  switch (event.key) {
+    //player keys
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+
+    //enemy keys
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      break;
+  }
+});
